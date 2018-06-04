38e9b92ff8e5bd4ee9435fd24cbc2bc2c21cbe98
aivensong <aiven.fs@gmail.com>
2018-06-04 13:40:27 -0400

    gofmt code

--- main.go
+++ main.go
@@ -1,102 +1,102 @@
 package main
 
 import (
-        "log"
-        "os"
-        "time"
-        "fmt"
-        "strings"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
-        "github.com/urfave/cli"
-        "github.com/google/gopacket/pcap"
+	"github.com/google/gopacket/pcap"
+	"github.com/urfave/cli"
 )
 
 var (
-        iface string
-        outpath string
-        bpf string
-        quiet bool = false
-        promisc bool = false
+	iface   string
+	outpath string
+	bpf     string
+	quiet   bool = false
+	promisc bool = false
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "nosygopher"
-  app.Usage = "sniff things"
-  app.Version = "0.0.1"
+	app := cli.NewApp()
+	app.Name = "nosygopher"
+	app.Usage = "sniff things"
+	app.Version = "0.0.1"
 
-  app.Flags = []cli.Flag {
-    cli.StringFlag{
-      Name:        "interface",
-      Value:       "en0",
-      Usage:       "comma-separated list of interface devices to sniff on (e.g. en0,bridge0)",
-      Destination:  &iface,
-    },
-    cli.StringFlag{
-      Name:        "outpath",
-      Usage:       "path to write pcap file to, if left empty will not write",
-      Destination:  &outpath,
-    },
-    cli.StringFlag{
-      Name:        "bpf",
-      Usage:       "berkeley packet filter string ('tcp and port 80')",
-      Destination:  &bpf,
-    },
-    cli.BoolFlag{
-      Name:        "quiet",
-      Usage:       "if present will not log to stdout",
-      Destination:  &quiet,
-    },
-    cli.BoolFlag{
-      Name:        "promiscuous",
-      Usage:       "capture in promiscuous mode",
-      Destination:  &promisc,
-    },
-  }
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "interface",
+			Value:       "en0",
+			Usage:       "comma-separated list of interface devices to sniff on (e.g. en0,bridge0)",
+			Destination: &iface,
+		},
+		cli.StringFlag{
+			Name:        "outpath",
+			Usage:       "path to write pcap file to, if left empty will not write",
+			Destination: &outpath,
+		},
+		cli.StringFlag{
+			Name:        "bpf",
+			Usage:       "berkeley packet filter string ('tcp and port 80')",
+			Destination: &bpf,
+		},
+		cli.BoolFlag{
+			Name:        "quiet",
+			Usage:       "if present will not log to stdout",
+			Destination: &quiet,
+		},
+		cli.BoolFlag{
+			Name:        "promiscuous",
+			Usage:       "capture in promiscuous mode",
+			Destination: &promisc,
+		},
+	}
 
-  app.Action = func(c *cli.Context) error {
-      ng := NosyGopher{
-          iface: iface,
-          outpath: outpath,
-          bpf: bpf,
-          quiet: quiet,
-          promisc: promisc,
-          snapshotLen: 1024,
-          timeout: 30 * time.Second,
-      }
-      err := ng.Sniff()
-      if err != nil {
-          return fmt.Errorf("nosy gopher has issues: %s", err.Error())
-      }
-      return nil
-  }
+	app.Action = func(c *cli.Context) error {
+		ng := NosyGopher{
+			iface:       iface,
+			outpath:     outpath,
+			bpf:         bpf,
+			quiet:       quiet,
+			promisc:     promisc,
+			snapshotLen: 1024,
+			timeout:     30 * time.Second,
+		}
+		err := ng.Sniff()
+		if err != nil {
+			return fmt.Errorf("nosy gopher has issues: %s", err.Error())
+		}
+		return nil
+	}
 
-  app.Commands = []cli.Command {
-    cli.Command {
-      Name: "list",
-      Usage: "list interfaces nosygopher can sniff",
-      Action: listInterfaces,
-    },
-  }
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:   "list",
+			Usage:  "list interfaces nosygopher can sniff",
+			Action: listInterfaces,
+		},
+	}
 
-  err := app.Run(os.Args)
-  if err != nil {
-    log.Fatal(err)
-  }
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func listInterfaces(c *cli.Context) error {
-  fmt.Println("Here's what nosygopher can sniff...")
+	fmt.Println("Here's what nosygopher can sniff...")
 
-  var devices []pcap.Interface
-  var err error
-  devices, err = pcap.FindAllDevs()
-  if err != nil {
-    return err
-  }
-  for i, device := range devices {
-    fmt.Printf("%d: %s\n", i+1, device.Name)
-  }
+	var devices []pcap.Interface
+	var err error
+	devices, err = pcap.FindAllDevs()
+	if err != nil {
+		return err
+	}
+	for i, device := range devices {
+		fmt.Printf("%d: %s\n", i+1, device.Name)
+	}
 
-  return nil
+	return nil
 }
--- nosygopher.go
+++ nosygopher.go
@@ -1,97 +1,101 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "os"
+	"fmt"
+	"os"
+	"time"
 
-    "github.com/google/gopacket"
-    "github.com/google/gopacket/pcap"
-    "github.com/google/gopacket/pcapgo"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/google/gopacket/pcapgo"
 )
 
 type NosyGopher struct {
-    iface []string
-    outpath, bpf string
-    quiet, promisc bool
-    snapshotLen int
-    timeout time.Duration
+	iface          []string
+	outpath, bpf   string
+	quiet, promisc bool
+	snapshotLen    int
+	timeout        time.Duration
 }
 
 type NGResult struct {
-  err error
-  packet gopacket.Packet
-  writer pcapgo.Writer
+	err    error
+	packet gopacket.Packet
+	writer pcapgo.Writer
 }
 
-func(ng *NosyGopher) Sniff() error {
-  var chans []<-chan NGResult
-  for _, dev := range ng.iface {
-    append(chans, ng.sniffDevice(dev))
-  }
+func (ng *NosyGopher) Sniff() error {
+	var chans []<-chan NGResult
+	for _, dev := range ng.iface {
+		append(chans, ng.sniffDevice(dev))
+	}
 
-  c := fanin(chans...)
-  for res := range c {
-    if !ng.quiet { fmt.Println(res.packet) }
-    if res.writer != nil { res.writer.WritePacket(res.packet.Metadata().CaptureInfo, packet.Data()) }
-  }
+	c := fanin(chans...)
+	for res := range c {
+		if !ng.quiet {
+			fmt.Println(res.packet)
+		}
+		if res.writer != nil {
+			res.writer.WritePacket(res.packet.Metadata().CaptureInfo, packet.Data())
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func (ng *NosyGopher) sniffDevice(dev string) <-chan NGResult {
-    fmt.Printf("nosy gopher is sniffing on %s...\n", dev)
-    c := make(chan NGResult)
+	fmt.Printf("nosy gopher is sniffing on %s...\n", dev)
+	c := make(chan NGResult)
 
-    go func() {
-      // Open device
-      handle, err := pcap.OpenLive(dev, int32(ng.snapshotLen), ng.promisc, ng.timeout)
-      if err != nil {
-        return NGResult{packet: nil, writer: nil, err: err}
-      }
-      defer handle.Close()
+	go func() {
+		// Open device
+		handle, err := pcap.OpenLive(dev, int32(ng.snapshotLen), ng.promisc, ng.timeout)
+		if err != nil {
+			return NGResult{packet: nil, writer: nil, err: err}
+		}
+		defer handle.Close()
 
-      // Set BPFFilter if present
-      if ng.bpf != "" {
-        if err := handle.SetBPFFilter(ng.bpf); err != nil {
-          return NGResult{packet: nil, writer: nil, err: err}
-        }
-      }
+		// Set BPFFilter if present
+		if ng.bpf != "" {
+			if err := handle.SetBPFFilter(ng.bpf); err != nil {
+				return NGResult{packet: nil, writer: nil, err: err}
+			}
+		}
 
-      // Create writer if outpath is set
-      var writer *pcapgo.Writer
-      var f *os.File
-      if ng.outpath != "" {
-          writer, f = ng.writer(handle)
-          defer f.Close()
-      }
+		// Create writer if outpath is set
+		var writer *pcapgo.Writer
+		var f *os.File
+		if ng.outpath != "" {
+			writer, f = ng.writer(handle)
+			defer f.Close()
+		}
 
-      packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-      for packet := range packetSource.Packets() {
-        c <- NGResult{packet: packet, writer: writer, err: nil}
-      }
-    }()
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			c <- NGResult{packet: packet, writer: writer, err: nil}
+		}
+	}()
 
-    return c
+	return c
 }
 
 // Creates file, writer and writes file header
 func (ng *NosyGopher) writer(handle *pcap.Handle) (*pcapgo.Writer, *os.File) {
-    f, _ := os.Create(ng.outpath)
-    w := pcapgo.NewWriter(f)
-    w.WriteFileHeader(uint32(ng.snapshotLen), handle.LinkType())
-    return w, f
+	f, _ := os.Create(ng.outpath)
+	w := pcapgo.NewWriter(f)
+	w.WriteFileHeader(uint32(ng.snapshotLen), handle.LinkType())
+	return w, f
 }
 
 // Variadic fanin function
 func fanin(inputs ...<-chan NGResult) <-chan NGResult {
-  agg := make(chan NGResult)
+	agg := make(chan NGResult)
 
-  for _, ch := range inputs {
-    go func(c chan NGResult) {
-      for msg := range c {
-        agg <- msg
-      }
-    }(ch)
-  }
+	for _, ch := range inputs {
+		go func(c chan NGResult) {
+			for msg := range c {
+				agg <- msg
+			}
+		}(ch)
+	}
 }
